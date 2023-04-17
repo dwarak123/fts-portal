8ee3f5aba44f4bd179516cf8152851f6c9588177
Dwarak Calayampundi <dwarak.calayampundi@gmail.com>
2023-04-17 20:27:01 +1000

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import { Component } from "react";
-import { Routes, Route, redirect } from "react-router-dom";
+
+import { Routes, Route } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { render } from "react-dom";
-import AuthService from "./services/auth.service";
-import IUser from "./types/user.type";
-
 import Login from "./components/login.component";
 import Register from "./components/register.component";
 import Home from "./components/home.component";
@@ -14,20 +10,7 @@ import Profile from "./components/profile.component";
 import BoardUser from "./components/board-user.component";
 import BoardModerator from "./components/board-moderator.component";
 import BoardAdmin from "./components/board-admin.component";
-import EventBus from "./common/EventBus";
-import Avatar from "react-avatar";
 import { CssBaseline } from "@mui/material";
-import authService from "./services/auth.service";
-const currentUser = authService.getCurrentUser();
-type Props = {};
-
-type State = {
-  showModeratorBoard: boolean | undefined;
-  showAdminBoard: boolean | undefined;
-  currentUser: IUser | undefined;
-  hour: number;
-};
-
 
 function App() {
   return (
